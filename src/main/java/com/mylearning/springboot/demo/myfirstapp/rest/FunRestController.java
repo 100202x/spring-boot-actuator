842f1a11273667832129be8b8a45a1093017ae68
Mohamed Infaz <mim.infaz.lk@gmail.com>
2024-09-22 23:23:20 +0300

    Acess the application.properties file

--- src/main/java/com/mylearning/springboot/demo/myfirstapp/rest/FunRestController.java
+++ src/main/java/com/mylearning/springboot/demo/myfirstapp/rest/FunRestController.java
@@ -1,11 +1,18 @@
 package com.mylearning.springboot.demo.myfirstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
     //expose '/' rest controller that return Hello world
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
@@ -20,6 +27,11 @@ public class FunRestController {
     @GetMapping("/fortune")
     public String getDailyFortune() {
         return "Get Daily Fortune!";
+    }
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Team name is "+teamName+" And coach is "+coachName;
     }
 
 }
